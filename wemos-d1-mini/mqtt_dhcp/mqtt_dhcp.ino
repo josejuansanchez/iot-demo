a071c1e32c7277672f0bedebb798db210b14d8d6
José Juan Sánchez <josejuan.sanchez@gmail.com>
2018-01-09 14:48:27 +0100

    Added sketch to use a dynamic ip with dhcp

---
+++ wemos-d1-mini/mqtt_dhcp/mqtt_dhcp.ino
@@ -0,0 +1,106 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define DHTPIN D4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// WiFi configuration
+#define WLAN_SSID       ""
+#define WLAN_PASS       ""
+
+// MQTT configuration
+#define MQTT_SERVER      "test.mosquitto.org"
+//#define MQTT_SERVER      "192.168.1.200"
+#define MQTT_SERVERPORT  1883
+#define MQTT_USERNAME    ""
+#define MQTT_KEY         ""
+#define MQTT_FEED_TEMP   "ies/aula20/temperature"
+#define MQTT_FEED_HUMI   "ies/aula20/humidity"
+
+// WiFi connection
+WiFiClient client;
+
+// MQTT connection
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_USERNAME, MQTT_KEY);
+
+// Feed to publish temperature
+Adafruit_MQTT_Publish temperatureFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_FEED_TEMP);
+
+// Feed to publish humidity
+Adafruit_MQTT_Publish humidityFeed = Adafruit_MQTT_Publish(&mqtt, MQTT_FEED_HUMI);
+
+//----------------------------------------------
+
+void connectWiFi();
+
+//----------------------------------------------
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("IoT demo");
+  dht.begin();
+  connectWiFi();
+  connectMQTT();
+}
+
+//----------------------------------------------
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+ 
+  temperatureFeed.publish(t);
+  humidityFeed.publish(h);
+}
+
+//----------------------------------------------
+
+void connectWiFi() {
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+//----------------------------------------------
+
+void connectMQTT() {
+  if (mqtt.connected())
+    return;
+
+  Serial.print("Connecting to MQTT... ");
+  while (mqtt.connect() != 0) {
+       Serial.println("Error. Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);
+  }
+}
+
